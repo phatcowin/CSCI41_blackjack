0807c7d26c40775d4a9c72a2abc1f6a1e516b8c0
eddiecolli94 <60409357+eddiecolli94@users.noreply.github.com>
2020-01-28 17:37:43 -0800

    Add files via upload

--- main.cc
+++ main.cc
@@ -6,9 +6,15 @@ using namespace std;
 
 int main() {
 	string input;
-	int players = 0;
+	vector<Player> players;
+	int play_num = 0;
 	cout << "BLACK JACK\N";
 	cout << "Please enter the number of players: " << endl;
-	cin >> players;
-	cin >> string >> endl;
+	cin >> play_num;
+
+	//create a vector of players that we can traverse to simulate turns
+	//when a player loses, cashes out/quits, or breaks the bank, they can then be removed from the vector
+	for(int i = 0; i < play_num; i++){
+		players.push_back();
+	}
 }
