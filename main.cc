106cbaf0b747b4b65d0d727f83f2faec49b25cb0
eddiecolli94 <60409357+eddiecolli94@users.noreply.github.com>
2020-01-28 17:31:40 -0800

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,14 @@
+//#include <cassert>
+#include <iostream>
+#include <Player.h>
+#include <Deck.h>
+using namespace std;
+
+int main() {
+	string input;
+	int players = 0;
+	cout << "BLACK JACK\N";
+	cout << "Please enter the number of players: " << endl;
+	cin >> players;
+	cin >> string >> endl;
+}
